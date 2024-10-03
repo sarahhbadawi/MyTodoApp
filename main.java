6f0fd296e436173d1ad4aae638818d4f6cf9b5db
Sarah Badawi <sbadawi1@emich.edu>
2024-10-03 02:12:56 +0000

    Attepting to create conflict again

--- main.java
+++ main.java
@@ -55,6 +55,6 @@ public class TodoList {
         // Display all items
         myTodoList.displayTodoItems();
 
-        System.out.println("Helloooooo!");
+        System.out.println("Helloooo!");
     }
 }
